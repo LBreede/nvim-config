53a9db87529ba80e9c69cd65b00e9a8ce49423b2
Lennart Breede <lennart.breede@gmail.com>
2026-09-01 22:29:24 -0700

    fix: skip cursor restoration in Git message buffers

--- init.lua
+++ init.lua
@@ -65,6 +65,10 @@ do
     desc = "Restore the last cursor position",
     group = vim.api.nvim_create_augroup("last-position", { clear = true }),
     callback = function(args)
+      if vim.tbl_contains({ "gitcommit", "gitrebase" }, vim.bo[args.buf].filetype) then
+        return
+      end
+
       local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
       if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(args.buf) then
         vim.api.nvim_win_set_cursor(0, mark)
